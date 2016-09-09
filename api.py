bf645c26e5569d736b08cda6a7b2111f8648f698
trase <trasewestbrook@gmail.com>
2016-09-09 00:21:25 -0500

    added data normalizatin to api

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-import json
+import json, numpy
 from keras.models import load_model
 
 
@@ -7,18 +7,34 @@ model = load_model('afknet.h5')
 
 app = Flask(__name__)
 
+def nonlin(x, deriv=False):
+	# if(deriv==True):
+	#     return x*(1-x)
+	# return 1/(1+np.exp(-x))
+	return numpy.tanh(x)
+
 def cmp(li):
 	if li[0] > li[1]:
 		#not cheating
-		return 1
+		return 0
 	#cheating
-	return 0
+	return 1
 
 @app.route('/', methods=['POST'])
 def predict():
 
-	data = request.get_json()
+	data = json.loads(request.form['points'])
+	data = numpy.array(data)
+	data = nonlin(data)
 
 	prediction = model.predict(data)
+	print(prediction)
 
-	return cmp(prediction)
+	out = []
+	for i in prediction:
+		out.append(cmp(i))
+
+	return json.dumps(out)
+
+if __name__ == '__main__':
+	app.run(debug=True)
